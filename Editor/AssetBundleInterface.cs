e005ac3be38f24ed29fe79be099dda3353c81f43
Mark Oliver Mints <mmints@uni-koblenz.de>
2019-11-18 16:38:22 +0100

    [Fix] typo

--- Editor/AssetBundleInterface.cs
+++ Editor/AssetBundleInterface.cs
@@ -2,18 +2,29 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace Editor
+public class AssetBundleInterface : MonoBehaviour
 {
-    public class AssetBundleInterface : MonoBehaviour
+    
+    public string GetPathToScene(string[] options, int index)
     {
-        public void BuildAllAssetBundles(string assetBundleDirectory)
+        // Can be usefull for late use
+        string[] paths = AssetDatabase.GetAssetPathsFromAssetBundle(options[index]);
+        if (paths.Length == 0)
         {
-            if(!Directory.Exists(assetBundleDirectory))
-            {
-                Directory.CreateDirectory(assetBundleDirectory);
-            }
-            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
-            Debug.Log("All Asset Bundles has been build and saved to: " + assetBundleDirectory);
+            return "Selected AssetBundle Is Not Assigned to a Scene";
         }
+        else {
+            return paths[0];
+        }
+    }
+
+    public void BuildAllAssetBundles(string assetBundleDirectory)
+    {
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+        Debug.Log("All Asset Bundles have been build and saved to: " + assetBundleDirectory);
     }
 }
